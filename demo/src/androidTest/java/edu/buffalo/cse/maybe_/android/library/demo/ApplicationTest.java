c438c0f544ca2ae165c51161b0e777531df1dd6b
xcv58 <chenyihonglove@gmail.com>
2015-10-15 23:52:55 -0400

    fix Travis CI

---
+++ demo/src/androidTest/java/edu/buffalo/cse/maybe_/android/library/demo/ApplicationTest.java
@@ -0,0 +1,13 @@
+package edu.buffalo.cse.maybe_.android.library.demo;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * Created by xcv58 on 10/15/15.
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ demo/src/test/java/MaybeServiceTest.java
@@ -0,0 +1,6 @@
+/**
+ * Created by ramyarao on 7/29/15.
+ */
+public class MaybeDemoTest {
+//TODO: check all methods with local unit test cases
+}
